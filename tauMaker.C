762907c176344d96dd720f0c22c7be669074360a
Sridhara Dasu <dasu@hep.wisc.edu>
2022-08-17 16:43:30 -0500

    Improved tau storage - creates png files now

--- tauMaker.C
+++ tauMaker.C
@@ -39,6 +39,10 @@ public:
     for(map<string, TH1F*>::iterator iter = histograms1D.begin(); iter != histograms1D.end(); ++iter)
       {
 	iter->second->Write();
+	TCanvas c;
+	iter->second->Draw();
+	string name = iter->first + ".png";
+	c.SaveAs(name.c_str());
       }
     tFile->Close();
   }
@@ -51,6 +55,28 @@ public:
 private:
   map<string, TH1F*> histograms1D;
   TFile *tFile;
+};
+
+class Tau {
+public:
+  // Constructor
+  Tau(TLorentzVector tauP4, TLorentzVector trackP4, int c, int nPr, int nPh, int nNH, double iso = 0.0) {
+    P4 = tauP4;
+    MaxTrackP4 = trackP4;
+    charge = c;
+    nProngs = nPr;
+    nPhotons = nPh;
+    nNHadrons = nNH;
+    isolation = iso;
+  }
+  // Data
+  TLorentzVector P4;
+  TLorentzVector MaxTrackP4;
+  int charge;
+  int nProngs;
+  int nPhotons;
+  int nNHadrons;
+  double isolation;
 };
 
 void tauMaker(const char *inputFile)
@@ -95,26 +121,30 @@ void tauMaker(const char *inputFile)
   histograms.Define("RecTauNG", "Reconstructed tau EFPhoton count", 30, 0.0, 30.0);
   histograms.Define("RecTauNN", "Reconstructed tau EFNHadron count", 30, 0.0, 30.0);
   histograms.Define("RecTauMs", "Reconstructed tau mass", 100, 0.0, 10.0);
-  histograms.Define("MchTauPT", "Visible tau P_{T} for good match", 100, 0.0, 100.0);
-  histograms.Define("WrgTauPT", "Visible tau P_{T} for wrong match", 100, 0.0, 100.0);
-  histograms.Define("WrgTauCh", "Visible tau charge for wrong match", 10, 0.0, 10.0);
   histograms.Define("DeltaR", "Average DeltaR between max track and other fragments in tau", 100, 0.0, 1.0);
+  histograms.Define("MchTauPT", "Visible tau P_{T} for good match", 100, 0.0, 100.0);
+  histograms.Define("MchTauCh", "Matched tau charge", 21, -10.0, 10.0);
+  histograms.Define("MchTauNPr", "Matched tau nProngs", 30, 0.0, 30.0);
+  histograms.Define("MchTauNPh", "Matched tau nPhotons", 30, 0.0, 30.0);
+  histograms.Define("MchTauNNH", "Matched tau nNHadrons", 30, 0.0, 30.0);
+  histograms.Define("MchTauIso", "Matched tau isolation ratio", 100, 0.0, 1.0);
   histograms.Define("MchPTRes", "Matched tau reco vs vis PT resolution", 100, -1.0, 1.0);
   histograms.Define("MchEtaRes", "Matched tau reco vs vis Eta resolution", 100, -1.0, 1.0);
   histograms.Define("MchPhiRes", "Matched tau reco vs vis Phi resolution", 100, -1.0, 1.0);
+  histograms.Define("WrgTauPT", "Visible tau P_{T} for wrong match", 100, 0.0, 100.0);
+  histograms.Define("WrgTauCh", "Wrongly matched tau charge", 21, -10.0, 10.0);
+  histograms.Define("WrgTauNPr", "Matched tau nProngs", 30, 0.0, 30.0);
+  histograms.Define("WrgTauNPh", "Matched tau nPhotons", 30, 0.0, 30.0);
+  histograms.Define("WrgTauNNH", "Matched tau nNHadrons", 30, 0.0, 30.0);
+  histograms.Define("WrgTauIso", "Wrongly matched tau isolation ratio", 100, 0.0, 1.0);
   histograms.Define("WrgPTRes", "Wrongly matched tau reco vs vis PT resolution", 100, -1.0, 1.0);
   histograms.Define("WrgEtaRes", "Wrongly matched tau reco vs vis Eta resolution", 100, -1.0, 1.0);
   histograms.Define("WrgPhiRes", "Wrongly matched tau reco vs vis Phi resolution", 100, -1.0, 1.0);
 
-  vector<TLorentzVector> genTauMList;
-  vector<TLorentzVector> genTauPList;
-  vector<TLorentzVector> visTauMList;
-  vector<TLorentzVector> visTauPList;
-  vector<TLorentzVector> mxTTauMList;
-  vector<TLorentzVector> mxTTauPList;
-  vector<TLorentzVector> recTauMList;
-  vector<TLorentzVector> recTauPList;
-  vector<TLorentzVector> recTauXList;
+  vector<Tau> genTaus;
+  vector<Tau> visTaus;
+  vector<Tau> recTaus;
+  vector<Tau> mchTaus;
 
   cout << "Number of entries = " << numberOfEntries << endl;
 
@@ -129,12 +159,10 @@ void tauMaker(const char *inputFile)
       // Sometimes there may ba a second pair of tau+ and tau- from the decay of a Z-boson
       // Collect all the particles
 
-      genTauMList.clear();
-      visTauMList.clear();
-      genTauPList.clear();
-      visTauPList.clear();
-      mxTTauMList.clear();
-      mxTTauPList.clear();
+      genTaus.clear();
+      visTaus.clear();
+      recTaus.clear();
+      mchTaus.clear();
 
       for (Int_t p = 0; p < branchParticles->GetEntries(); ++p) {
 	GenParticle *particle = (GenParticle*) branchParticles->At(p);
@@ -183,45 +211,40 @@ void tauMaker(const char *inputFile)
 	  GenParticle *theMother = (GenParticle*) branchParticles->At(particle->M1);
 	  double chargedDaughterPT = 0;
 	  GenParticle *theDaughter = 0;
-	  for (Int_t d = (theMother->D1 + 1); d <= theMother->D2; d++) {
+	  int nProngs = 0;
+	  int nPhotons = 0;
+	  int nNHadrons = 0;
+	  for (Int_t d = theMother->D1; d <= theMother->D2; d++) {
 	    GenParticle *daughter = (GenParticle*) branchParticles->At(d);
-	    if ((abs(daughter->PID) == 211 || abs(daughter->PID) == 321 || abs(daughter->PID) == 11 || abs(daughter->PID) == 13) && daughter->PT > chargedDaughterPT) {
-	      theDaughter = daughter;
-	      chargedDaughterPT = daughter->PT;
+	    if (theDaughter == 0) theDaughter = daughter;
+	    if ((abs(daughter->PID) == 211 || abs(daughter->PID) == 321 || abs(daughter->PID) == 11 || abs(daughter->PID) == 13)) {
+	      nProngs++;
+	      if(daughter->PT > chargedDaughterPT) {
+		theDaughter = daughter;
+		chargedDaughterPT = daughter->PT;
+	      }
+	    }
+	    else if(abs(daughter->PID) == 22) {
+	      nPhotons++;
+	    }
+	    else if(abs(daughter->PID) != 12 && abs(daughter->PID) != 14 && abs(daughter->PID) != 16) {
+	      nNHadrons++; // Catch all
 	    }
 	  }
-	  if (tau->PID == 15) {
-	    genTauMList.push_back(TLorentzVector(tau->P4()));
-	    visTauMList.push_back(TLorentzVector(tau->P4() - particle->P4()));
-	    if(chargedDaughterPT > 0) mxTTauMList.push_back(theDaughter->P4());
-	  }
-	  else {
-	    genTauPList.push_back(TLorentzVector(tau->P4()));
-	    visTauPList.push_back(TLorentzVector(tau->P4() - particle->P4()));
-	    if(chargedDaughterPT > 0) mxTTauPList.push_back(theDaughter->P4());
-	  }
+	  int charge = tau->Charge;
+	  genTaus.push_back(Tau(tau->P4(), theDaughter->P4(), charge, nProngs, nPhotons, nNHadrons));
+	  visTaus.push_back(Tau((tau->P4() - particle->P4()), theDaughter->P4(), charge, nProngs, nPhotons, nNHadrons));
 	}
       }
 
-      for (Int_t i = 0; i < genTauMList.size(); i++) {
-	histograms.Fill("GenTauPT", genTauMList[i].Pt());
-        histograms.Fill("MxTTauPT", mxTTauMList[i].Pt());
-        histograms.Fill("TFrTauPT", mxTTauMList[i].Pt() / genTauMList[i].Pt());
-	histograms.Fill("VisTauPT", visTauMList[i].Pt());
-      }
-      for (Int_t i = 0; i < genTauPList.size(); i++) {
-	histograms.Fill("GenTauPT", genTauPList[i].Pt());
-        histograms.Fill("MxTTauPT", mxTTauPList[i].Pt());
-        histograms.Fill("TFrTauPT", mxTTauPList[i].Pt() / genTauPList[i].Pt());
-	histograms.Fill("VisTauPT", visTauPList[i].Pt());
+      for (Int_t i = 0; i < genTaus.size(); i++) {
+	histograms.Fill("GenTauPT", genTaus[i].P4.Pt());
+        histograms.Fill("MxTTauPT", genTaus[i].MaxTrackP4.Pt());
+        histograms.Fill("TFrTauPT", genTaus[i].MaxTrackP4.Pt() / genTaus[i].P4.Pt());
+	histograms.Fill("VisTauPT", visTaus[i].P4.Pt());
       }
 
       // Reconstruct taus using EFlow tracks, photons and neutral hadrons
-
-      recTauMList.clear();
-      recTauPList.clear();
-      recTauXList.clear();
-
       // Consider every charged track above 5 GeV PT - label at as a potential tau remnant
       // Add in all tracks and clusters within 0.3 of the track
       // Compute sum charge of the tracks
@@ -237,11 +260,11 @@ void tauMaker(const char *inputFile)
 	  bool selected = true;
 	  recoTau.SetPtEtaPhiM(track->PT, track->Eta, track->Phi, track->Mass);
 	  double isolation = 0;
-	  int charge = track->PID / abs(track->PID);
+	  int charge = track->Charge;
 	  int nProngs = 1;
 	  double ptSum = 0;
 	  double deltaRxPTSum = 0;
-	  if (entry < 10) cout << "Max: EFTrack[" << t << "] = (" << track->PT << ", " << track->Eta << ", " << track->Phi << ", " << track->Mass << ")";
+	  // if (entry < 10) cout << "Max: EFTrack[" << t << "] = (" << track->PT << ", " << track->Eta << ", " << track->Phi << ", " << track->Mass << ")";
 	  for (Int_t a = 0; a < branchEFTracks->GetEntries(); ++a) {
 	    if (a != t) {
 	      Track *fragment = (Track*) branchEFTracks->At(a);
@@ -249,7 +272,7 @@ void tauMaker(const char *inputFile)
 	      double deltaR = recoTau.DeltaR(fragmentP4);
 	      if(deltaR < 0.5) {
 		if (fragment->PT > track->PT) {
-		  if (entry < 10) cout << " - Deselected" << endl;
+		  // if (entry < 10) cout << " - Deselected" << endl;
 		  selected = false;
 		  break;
 		}
@@ -258,8 +281,8 @@ void tauMaker(const char *inputFile)
 		  deltaRxPTSum += (deltaR * fragment->PT);
 		  if (deltaR < 0.3) {
 		    recoTau += fragmentP4;
-		    if (entry < 10) cout << endl << "Frg: EFTrack[" << a << "] = (" << fragment->PT << ", " << fragment->Eta << ", " << fragment->Phi << ", " << fragment->Mass << ")";
-		    charge += (fragment->PID / abs(fragment->PID));
+		    // if (entry < 10) cout << endl << "Frg: EFTrack[" << a << "] = (" << fragment->PT << ", " << fragment->Eta << ", " << fragment->Phi << ", " << fragment->Mass << ")";
+		    charge += fragment->Charge;
 		    nProngs += 1;
 		  }
 		  else {
@@ -306,7 +329,7 @@ void tauMaker(const char *inputFile)
 	    }
 	  }
 	  if (selected) {
-	    if (entry < 10) cout << endl << "SelRecoTau = (" << recoTau.Pt() << ", " << recoTau.Eta() << ", " << recoTau.Phi() << ", "<< recoTau.M() << ")" << endl;
+	    // if (entry < 10) cout << endl << "SelRecoTau = (" << recoTau.Pt() << ", " << recoTau.Eta() << ", " << recoTau.Phi() << ", "<< recoTau.M() << ")" << endl;
 	    if (ptSum > 0) {
 	      double deltaR = deltaRxPTSum / ptSum;
 	      histograms.Fill("DeltaR", deltaR);
@@ -320,99 +343,42 @@ void tauMaker(const char *inputFile)
 	      histograms.Fill("RecTauIR", isolation / recoTau.Pt());
 	      histograms.Fill("RecTauCh", charge);
 	      histograms.Fill("RecTauMs", recoTau.M());
-	      if(charge == -1) {
-		recTauMList.push_back(recoTau);
-	      }
-	      else if(charge == 1) {
-		recTauPList.push_back(recoTau);
-	      }
-	      else {
-		recTauXList.push_back(recoTau);
-	      }
+	      recTaus.push_back(Tau(recoTau, track->P4(), charge, nProngs, nPhotons, nNHadrons, isolation));
 	    }
 	  } // Selected objects
 	} // Those above 5 GeV
       } // All tracks
 
-      /*
-      for (Int_t i = 0; i < recTauMList.size(); i++) {
-	histograms.Fill("RecTauPT", recTauMList[i].Pt());
-      }
-      for (Int_t i = 0; i < recTauPList.size(); i++) {
-	histograms.Fill("RecTauPT", recTauPList[i].Pt());
-      }
-      for (Int_t i = 0; i < recTauXList.size(); i++) {
-	histograms.Fill("RecTaxPT", recTauXList[i].Pt());
-      }
-      */
-
-      for (Int_t v = 0; v < visTauMList.size(); v++) {
+      for (Int_t v = 0; v < visTaus.size(); v++) {
 	// Find correct sign match
-	for (Int_t r = 0; r < recTauMList.size(); r++) {
-	  double deltaR = visTauMList[v].DeltaR(recTauMList[r]);
+	for (Int_t r = 0; r < recTaus.size(); r++) {
+	  double deltaR = visTaus[v].P4.DeltaR(recTaus[r].P4);
 	  if (deltaR < 0.3) {
-	    histograms.Fill("MchTauPT", visTauMList[v].Pt());
-	    histograms.Fill("MchPTRes", (recTauMList[r].Pt() - visTauMList[v].Pt()) / visTauMList[v].Pt());
-	    histograms.Fill("MchEtaRes", (recTauMList[r].Eta() - visTauMList[v].Eta()) / visTauMList[v].Eta());
-	    histograms.Fill("MchPhiRes", (recTauMList[r].Phi() - visTauMList[v].Phi()) / visTauMList[v].Phi());
+	    if(visTaus[v].charge == recTaus[r].charge) {
+	      mchTaus.push_back(recTaus[r]);
+	      histograms.Fill("MchTauPT", visTaus[v].P4.Pt());
+	      histograms.Fill("MchTauCh", recTaus[r].charge);
+	      histograms.Fill("MchTauIso", recTaus[r].isolation / recTaus[r].P4.Pt());
+	      histograms.Fill("MchTauNPr", recTaus[r].nProngs);
+	      histograms.Fill("MchTauNPh", recTaus[r].nPhotons);
+	      histograms.Fill("MchTauNNH", recTaus[r].nNHadrons);
+	      histograms.Fill("MchPTRes", (recTaus[r].P4.Pt() - visTaus[v].P4.Pt()) / visTaus[v].P4.Pt());
+	      histograms.Fill("MchEtaRes", (recTaus[r].P4.Eta() - visTaus[v].P4.Eta()) / visTaus[v].P4.Eta());
+	      histograms.Fill("MchPhiRes", (recTaus[r].P4.Phi() - visTaus[v].P4.Phi()) / visTaus[v].P4.Phi());
+	    }
+	    else {
+	      histograms.Fill("WrgTauPT", visTaus[v].P4.Pt());
+	      histograms.Fill("WrgTauCh", recTaus[r].charge);
+	      histograms.Fill("WrgTauIso", recTaus[r].isolation / recTaus[r].P4.Pt());
+	      histograms.Fill("WrgTauNPr", recTaus[r].nProngs);
+	      histograms.Fill("WrgTauNPh", recTaus[r].nPhotons);
+	      histograms.Fill("WrgTauNNH", recTaus[r].nNHadrons);
+	      histograms.Fill("WrgPTRes", (recTaus[r].P4.Pt() - visTaus[v].P4.Pt()) / visTaus[v].P4.Pt());
+	      histograms.Fill("WrgEtaRes", (recTaus[r].P4.Eta() - visTaus[v].P4.Eta()) / visTaus[v].P4.Eta());
+	      histograms.Fill("WrgPhiRes", (recTaus[r].P4.Phi() - visTaus[v].P4.Phi()) / visTaus[v].P4.Phi());
+	    }
 	    break;
 	  }
-	}
-        // Find wrong sign match                                                                                                                                             
-        for (Int_t r = 0; r < recTauPList.size(); r++) {
-          double deltaR = visTauMList[v].DeltaR(recTauPList[r]);
-          if (deltaR < 0.3) {
-            histograms.Fill("WrgTauPT", visTauMList[v].Pt());
-	    histograms.Fill("WrgPTRes", (recTauPList[r].Pt() - visTauMList[v].Pt()) / visTauMList[v].Pt());
-	    histograms.Fill("WrgEtaRes", (recTauPList[r].Eta() - visTauMList[v].Eta()) / visTauMList[v].Eta());
-	    histograms.Fill("WrgPhiRes", (recTauPList[r].Phi() - visTauMList[v].Phi()) / visTauMList[v].Phi());
-	    break;
-          }
-	}
-        for (Int_t r = 0; r < recTauXList.size(); r++) {
-          double deltaR = visTauMList[v].DeltaR(recTauXList[r]);
-          if (deltaR < 0.3) {
-            histograms.Fill("WrgTauPT", visTauMList[v].Pt());
-	    histograms.Fill("WrgPTRes", (recTauXList[r].Pt() - visTauMList[v].Pt()) / visTauMList[v].Pt());
-	    histograms.Fill("WrgEtaRes", (recTauXList[r].Eta() - visTauMList[v].Eta()) / visTauMList[v].Eta());
-	    histograms.Fill("WrgPhiRes", (recTauXList[r].Phi() - visTauMList[v].Phi()) / visTauMList[v].Phi());
-	    break;
-          }
-	}
-      }
-
-      for (Int_t v = 0; v < visTauPList.size(); v++) {
-	// Find correct sign match
-	for (Int_t r = 0; r < recTauPList.size(); r++) {
-	  double deltaR = visTauPList[v].DeltaR(recTauPList[r]);
-	  if (deltaR < 0.3) {
-	    histograms.Fill("MchTauPT", visTauPList[v].Pt());
-	    histograms.Fill("MchPTRes", (recTauPList[r].Pt() - visTauPList[v].Pt()) / visTauPList[v].Pt());
-	    histograms.Fill("MchEtaRes", (recTauPList[r].Eta() - visTauPList[v].Eta()) / visTauPList[v].Eta());
-	    histograms.Fill("MchPhiRes", (recTauPList[r].Phi() - visTauPList[v].Phi()) / visTauPList[v].Phi());
-	    break;
-	  }
-	}
-        // Find wrong sign match                                                                                                                                             
-        for (Int_t r = 0; r < recTauMList.size(); r++) {
-          double deltaR = visTauPList[v].DeltaR(recTauMList[r]);
-          if (deltaR < 0.3) {
-            histograms.Fill("WrgTauPT", visTauPList[v].Pt());
-	    histograms.Fill("WrgPTRes", (recTauMList[r].Pt() - visTauPList[v].Pt()) / visTauPList[v].Pt());
-	    histograms.Fill("WrgEtaRes", (recTauMList[r].Eta() - visTauPList[v].Eta()) / visTauPList[v].Eta());
-	    histograms.Fill("WrgPhiRes", (recTauMList[r].Phi() - visTauPList[v].Phi()) / visTauPList[v].Phi());
-	    break;
-          }
-	}
-        for (Int_t r = 0; r < recTauXList.size(); r++) {
-          double deltaR = visTauPList[v].DeltaR(recTauXList[r]);
-          if (deltaR < 0.3) {
-            histograms.Fill("WrgTauPT", visTauPList[v].Pt());
-	    histograms.Fill("WrgPTRes", (recTauXList[r].Pt() - visTauPList[v].Pt()) / visTauPList[v].Pt());
-	    histograms.Fill("WrgEtaRes", (recTauXList[r].Eta() - visTauPList[v].Eta()) / visTauPList[v].Eta());
-	    histograms.Fill("WrgPhiRes", (recTauXList[r].Phi() - visTauPList[v].Phi()) / visTauPList[v].Phi());
-	    break;
-          }
 	}
       }
 
